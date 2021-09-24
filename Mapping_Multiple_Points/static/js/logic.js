7551950f672e08c23f81f89f41b7f604cdfd0924
Kathy Bauman <kathybmans@gmail.com>
2021-09-24 18:06:49 -0500

    updating index, logic, cities

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -3,16 +3,26 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 // L.map object, mapid from id in index.html, coordinates with zoom level 14 
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// 13.4.1
-//  Add a marker to the map for Los Angeles, California.  300 meter radius, yellow circle
-// black line around circle
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
+// 13.4.2
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city. add a popup
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: (city.population-200000)/100000,
+        fillColor: 'orange',
+        color: 'orange',
+        weight: 4
+    } )
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
 
 
 
