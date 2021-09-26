074f682869f6f984ae9f5e72b87495521415c461
Kathy Bauman <kathybmans@gmail.com>
2021-09-26 01:00:31 -0500

    changing logic.js

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,14 +2,14 @@
 console.log("working");
 
 
-// 13.5.2 and 13.5.3
+// // 13.5.2 and 13.5.3 
 // Create the map object with a center and zoom level.
 // L.map object, mapid from id in index.html, coordinates with zoom level 2
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
 
 
-// // Add GeoJSON data.
+// // 13.5.2 Add GeoJSON data.
 // let sanFranAirport =
 // {"type":"FeatureCollection","features":[{
 //     "type":"Feature",
@@ -29,7 +29,7 @@ let map = L.map('mapid').setView([30, 30], 2);
 //             "coordinates":[-122.375,37.61899948120117]}}
 // ]};
 
-// // Grabbing our GeoJSON data. using pointToLayer
+// // 13.5.2 Grabbing our GeoJSON data. using pointToLayer
 // // pass each GeoJSON feature as feature and latitue and long as latlng
 // // bindPopup returns the city
 // L.geoJson(sanFranAirport, {
@@ -45,7 +45,7 @@ let map = L.map('mapid').setView([30, 30], 2);
 // }).addTo(map);
 
 
-// // Grabbing our GeoJSON data. using onEachFeature
+// // 13.5.2 Grabbing our GeoJSON data. using onEachFeature
 // L.geoJson(sanFranAirport, {
 //   onEachFeature: function(feature, layer) {
 //     console.log(layer);
@@ -56,21 +56,49 @@ let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 // tileLayer method with API URL for accessToken and OpenStreetMap URL
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Then we add our 'streets' tile layer to the map.
-streets.addTo(map);
+// 13.5.4 We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// 13.5.3
+// 13.5.4 Create a base layer that holds both maps. Both tileLayer maps
+// Street, Dark keys set the text,  streets, dark are styles from tileLayer
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// 13.5.4 alternative map object without .setView
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// 13.5.4 Pass our map layers into our layers control and add the layers control to the map.
+// little icon (Layers Control) pops up in top right to show the layers selection
+L.control.layers(baseMaps).addTo(map);
+
+
+
+// Then we add our 'streets' tile layer to the map.
+// streets.addTo(map);
+
+// 13.5.3 and 13.5.4
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/ckbauman/Mapping_Earthquakes/main/majorAirports.json";
 
 
-// Grabbing our GeoJSON data. with popup info
+// 13.5.3 Grabbing our GeoJSON data. WITH popup info
 d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
@@ -83,17 +111,12 @@ L.geoJson(data, {
 });
 
 
-// // Grabbing our GeoJSON data. without popup info
+// // 13.5.3 and 13.5.4 Grabbing our GeoJSON data. WITHOUT popup info
 // d3.json(airportData).then(function(data) {
 //   console.log(data);
 // // Creating a GeoJSON layer with the retrieved data.
 // L.geoJson(data).addTo(map);
 // });
-
-
-
-
-
 
 
 // // Then we add our 'graymap' tile layer to the map.
